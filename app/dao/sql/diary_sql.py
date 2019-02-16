b8859e071a724bd60d279aaedc9ca285321e6100
郝小龙 <1337970681@qq.com>
2019-02-16 18:23:59 +0800

    增加 getDiaryUserIcon、getDiaryItem、getDiaryTitle   SQL语句 , 提交人：郝小龙

--- app/dao/sql/diary_sql.py
+++ app/dao/sql/diary_sql.py
@@ -16,5 +16,13 @@ diary_sql={
     "getDiaryInfo02":"select stage, public_date diary_date ,diary_content ,\
                       (SELECT group_concat(di.diary_img) FROM diary_img di WHERE di.diary_content_id = dc.id) diary_img\
                       from diary_content dc where diary_id={id}",
-    "writeDiary":""
+    "writeDiary":"",
+    "getDiaryUserIcon":"select diary.id, user_icon.icon from diary INNER JOIN `user` INNER JOIN user_icon\
+                        on diary.user_id=`user`.id and `user`.user_icon_id=user_icon.id limit 12",
+    "getDiaryItem":"SELECT d.id diary_id , ui.icon , d.diary_title , s.`name` style_name ,d.company ,\
+                        dc.diary_content,(SELECT group_concat(di.diary_img) FROM diary_img di WHERE di.diary_content_id = dc.id and dc.diary_id=d.id) diary_img \
+                        FROM `user` u INNER JOIN user_icon ui INNER JOIN diary d INNER JOIN style s INNER JOIN renovation_type rt \
+                        INNER JOIN diary_content dc on u.user_icon_id=ui.id and d.user_id=u.id and d.style_id=s.id and\
+                        d.renovation_type_id=rt.id and d.id=dc.diary_id where dc.stage='前期准备' and d.id={diary_id}",
+    "getDiaryTitle":"select d.id diary_id ,d.diary_title from diary d limit 6"
 }
