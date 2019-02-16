db061a1155ac3742cdb4ecdcd45766dd49d5ca30
郝小龙 <1337970681@qq.com>
2019-02-16 18:27:50 +0800

    增加getStrategyTitle，郝小龙

--- app/dao/sql/strategy_sql.py
+++ app/dao/sql/strategy_sql.py
@@ -7,6 +7,7 @@ strategy_sql={
                         on s.id=si.strategy_id && s.id = sc.strategy_id",
     "getStrategyDetail":"select s.strategy_title, s.public_date, s.author, sc.lead ,sc.strategy_content\
                         from strategy s inner join strategy_img si INNER JOIN strategy_content sc \
-                        on s.id=si.strategy_id && s.id = sc.strategy_id where s.id={id}"
+                        on s.id=si.strategy_id && s.id = sc.strategy_id where s.id={id}",
+    "getStrategyTitle":"select s.id strategy_id ,s.strategy_title from strategy s limit 6"
 
 }
